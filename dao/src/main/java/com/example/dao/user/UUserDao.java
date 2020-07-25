d523d6504e7ba6c207248473704d64a642fd1aa5
lanchengx@qq.com <sj13350803128>
2020-07-25 21:13:43 +0800

    init

---
+++ dao/src/main/java/com/example/dao/user/UUserDao.java
@@ -0,0 +1,14 @@
+package com.example.dao.user;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.model.user.UUser;
+
+/**
+ * 用户表(UUser)表数据库访问层
+ *
+ * @author lanchengx
+ * @since 2020-07-25 17:12:10
+ */
+public interface UUserDao extends BaseMapper<UUser> {
+
+}
